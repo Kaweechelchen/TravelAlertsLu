4fe47a826a3a17198816b5c1bbe5426e9aedda6c
Thierry Degeling <thierry.degeling@gmail.com>
2014-09-18 22:13:00 +0100

    Added twitter library
    now tweeting problems on the tracks =D

--- travelalerts.php
+++ travelalerts.php
@@ -1,5 +1,21 @@
 <?php
 
+    require_once('TwitterAPIExchange.php');
+
+    $twitterSettings = array(
+        'consumer_key'              => getenv('apikey'),
+        'consumer_secret'           => getenv('apisecret'),
+        'oauth_access_token'        => getenv('accesstoken'),
+        'oauth_access_token_secret' => getenv('accesstokensecret')
+    );
+
+    /** URL for REST request, see: https://dev.twitter.com/docs/api/1.1/ **/
+    $url = 'https://api.twitter.com/1.1/statuses/update.json';
+    $requestMethod = 'POST';
+
+    /** Perform a POST request and echo the response **/
+    $twitter = new TwitterAPIExchange( $twitterSettings );
+
     $xml = simplexml_load_file( 'http://mobile.cfl.lu/bin/help.exe/enl?tpl=rss_feed_global' );
 
     $data   = json_decode(json_encode( $xml ), true);
@@ -24,11 +40,19 @@
 
         if ( ! strpos( strtolower( $title ), 'work:' ) ) {
 
-            $message = substr( $lines[ 2 ], 0, 135) . ' #cfl';
+            $message = substr( $lines[ 2 ], 0, 140);
 
             if( ! exec('grep '.escapeshellarg( $lines[ 1 ] . ' ' . $message ). ' ./'.$file)) {
 
-                echo $message . PHP_EOL;
+                /** POST fields required by the URL above. See relevant docs as above **/
+                $postfields = array(
+                        'status' => $message
+                    );
+
+                echo $twitter->buildOauth( $url, $requestMethod )
+                             ->setPostfields( $postfields )
+                             ->performRequest();
+
                 file_put_contents(
                     $file,
                     $lines[ 1 ] . ' ' . $message.PHP_EOL,
@@ -44,5 +68,16 @@
     }
 
     if ( $count == 0 ) {
-        echo "A good service has now resumed on all the linees. If not, please give us a heads up #cfl";
+
+        $message = 'A good service has now resumed on all linees. If not, please give us a heads up';
+
+        /** POST fields required by the URL above. See relevant docs as above **/
+        $postfields = array(
+                'status' => $message
+            );
+
+        echo $twitter->buildOauth( $url, $requestMethod )
+                     ->setPostfields( $postfields )
+                     ->performRequest();
+
     }
