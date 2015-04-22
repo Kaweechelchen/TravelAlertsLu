877c0116f597f257305915eb23a75c29c4c3904e
Thierry Degeling <thierry@syn2cat.lu>
2015-04-22 12:50:17 +0200

    most of the time the other company names are used just before a comma.., fixing that

--- app/travelAlertsLu/IssueHelpers.php
+++ app/travelAlertsLu/IssueHelpers.php
@@ -323,9 +323,9 @@
         'train traffic'                     =>  'traffic',
         'train service'                     =>  'service',
         '–'                                 =>  "➡️",
-        'SNCB '                             =>  "@SNCB ",
-        'SNCF '                             =>  "@SNCF ",
-        'DB '                               =>  "@DB_Bahn ",
+        'SNCB'                              =>  "@SNCB",
+        'SNCF'                              =>  "@SNCF",
+        'DB'                                =>  "@DB_Bahn",
         ', '                                =>  "\n"
       );
 
