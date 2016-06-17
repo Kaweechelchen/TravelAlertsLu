c0d6461994eb699dc91098931ef41f076d8af2a4
Thierry Degeling <thierry@syn2cat.lu>
2016-06-17 16:50:40 +0200

    yet another try to make things happen

--- src/app.js
+++ src/app.js
@@ -1,8 +1,30 @@
-var koa = require('koa');
-var app = koa();
+'use strict';
+const messages = require('./controllers/messages');
+const compress = require('koa-compress');
+const logger = require('koa-logger');
+const serve = require('koa-static');
+const route = require('koa-route');
+const koa = require('koa');
+const path = require('path');
+const app = module.exports = koa();
 
-app.use(function *(){
-    this.body = 'Hello wurld ssss';
-});
+// Logger
+app.use(logger());
 
-app.listen(8080);
+app.use(route.get('/', messages.home));
+app.use(route.get('/messages', messages.list));
+app.use(route.get('/messages/:id', messages.fetch));
+app.use(route.post('/messages', messages.create));
+app.use(route.get('/async', messages.delay));
+app.use(route.get('/promise', messages.promise));
+
+// Serve static files
+app.use(serve(path.join(__dirname, 'public')));
+
+// Compress
+app.use(compress());
+
+if (!module.parent) {
+  app.listen(3000);
+  console.log('listening on port 3000');
+}
